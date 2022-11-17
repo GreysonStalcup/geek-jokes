ad876eec74eee58cd1f9d975def30dbac492423e
Greyson Stalcup <g_stalcup@hotmail.com>
2022-11-17 15:29:54 -0800

    fix link and fix readme

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,7 +62,7 @@ export default function Home() {
         
       </main>
       <footer className={styles.footer}>
-        <Link href="https://github.com/greysonstalcup" target={"blank"}><AiFillGithub size={35} /></Link>
+        <Link href="https://github.com/GreysonStalcup/geek-jokes" target={"blank"}><AiFillGithub size={35} /></Link>
       </footer>  
     </div>
   )
